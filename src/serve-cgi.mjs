e38ea0ab39b54c770c48f0263c14bdc50d4530cb
Mike <kenr.mk@gmail.com>
2018-02-26 17:07:40 +0100

    enabled POST in PHP CGI

--- src/serve-cgi.mjs
+++ src/serve-cgi.mjs
@@ -1,15 +1,24 @@
+import urlModule from 'url'
 import cp from 'child_process'
 import util from 'util'
+import {version} from './util.mjs'
 
+
+var SERVER_SOFTWARE = `Node/${process.version} Anchora/${version}`
 
 // BEWARE! Following code is an experimental implementation of CGI interface for
 // running PHP and other scripting languages. Currently tightly coupled and not
 // very well tested. Help and contributions are welcomed.
 
-// This only supports GET requests. POST is to be solved sometime in the future.
 export async function serveCgi(req, res, sink, desc, cgiPath) {
-	var env = this.createCgiEnv(req, res, sink, desc)
-	var cgi = cp.spawn(cgiPath, {env})
+	var env = this.createCgiEnv(req, sink, desc)
+	var cgi = cp.spawn(cgiPath, [], {env})
+
+	// POST body is piped to 'stdin' of the CGI.
+	if (req.readable)
+		req.pipe(cgi.stdin)
+	else if (req.body)
+		cgi.stdin.end(req.body)
 
 	var stdout = ''
 	var onData = buffer => {
@@ -21,10 +30,10 @@ export async function serveCgi(req, res, sink, desc, cgiPath) {
 		else
 			headerSeparator = '\n\n'
 		if (headerSeparator !== undefined) {
-			stdout = undefined
 			// Parse headers out of the stdout dump we've collected so far and write
 			// it to the sink along with following first chunk of body.
 			this.parseAndSendCgiHeaders(sink, stdout, headerSeparator)
+			stdout = undefined
 			// Remove this header-parsing listener and start piping straight into sink.
 			cgi.stdout.removeListener('data', onData)
 			cgi.stdout.pipe(sink)
@@ -32,77 +41,99 @@ export async function serveCgi(req, res, sink, desc, cgiPath) {
 	}
 
 	cgi.stdout.on('data', onData)
+
+	var onError = err => this.serveError(sink, 500, err)
+	cgi.stderr.once('data', onError)
+
+	cgi.once('close', code => {
+        if (code)
+            console.error(`CGI exited with code ${code}`)
+		cgi.stderr.removeListener('data', onError)
+    })
+
 }
 
 
 export function parseAndSendCgiHeaders(sink, stdout, headerSeparator) {
+	// Locate end of header / beggining of body
 	var separatorLength = headerSeparator.length
 	let index = stdout.indexOf(headerSeparator)
-
+	// Parse headers created inside the script and apply them the response Node is creating.
 	let headers = stdout.slice(0, index).split(separatorLength === 4 ? '\r\n' : '\n')
-	var header
 	for (var i = 0; i < headers.length; i++) {
-		header = headers[i].split(':')
-		sink.setHeader(header[0], header[1] || '')
+		var [name, value] = headers[i].split(':')
+		name = name.toLowerCase()
+		// Status header has to be ignored and used for assigning res.statusCode.
+		if (name === 'status') {
+			sink.statusCode = parseInt(value)
+			continue
+		}
+		sink.setHeader(name, value || '')
 	}
-
+	// Script might return chunked response instead of exactly sized one.
 	if (!sink.getHeader('content-length'))
 		sink.setHeader('transfer-encoding', 'chunked')
 	sink.writeHead(sink.statusCode)
-
+	// Also write the beggining of the body that remains to the sink. 
 	let bodyChunk = stdout.slice(index + separatorLength)
 	sink.write(bodyChunk)
 }
 
 
-export function createCgiEnv(req, res, sink, desc) {
-	var url = req.url
-	var SCRIPT_NAME = url
-	var QUERY_STRING = ''
-	if (url.includes('?')) {
-		let index = url.indexOf('?')
-		QUERY_STRING = url.slice(index + 1)
-		SCRIPT_NAME = url.slice(0, index)
-	}
-	SCRIPT_NAME = decodeURI(SCRIPT_NAME)
-	if (req.httpVersion) {
-		var SERVER_PROTOCOL = `HTTP/${req.httpVersion}`
-	}
+export function createCgiEnv(req, sink, desc) {
+	var uri = urlModule.parse(req.url)
+
 	if (req.socket.constructor.name === 'TLSSocket')
 		var REQUEST_SCHEME = 'https'
 	else
 		var REQUEST_SCHEME = 'http'
-	var DOCUMENT_ROOT = this.root.replace(/\\/g, '\/')
-	var SCRIPT_FILENAME = desc.fsPath.replace(/\\/g, '\/')
+
+	var root = this.root.replace(/\\/g, '\/')
+
 	var env = {
 		REDIRECT_STATUS: 200,
 		GATEWAY_INTERFACE: 'CGI/1.1',
-		QUERY_STRING,
-		SERVER_PROTOCOL,
-		REQUEST_SCHEME,
-		REQUEST_URI: url,
-		DOCUMENT_ROOT,
-		CONTEXT_DOCUMENT_ROOT: DOCUMENT_ROOT,
-		SCRIPT_NAME,
-		SCRIPT_FILENAME,
+		SERVER_PROTOCOL: `HTTP/${req.httpVersion}`,
+		//REQUEST_SCHEME,
+		REQUEST_METHOD: req.method,
+		QUERY_STRING: uri.query || '',
+		REQUEST_URI: uri.href,
+		SCRIPT_NAME: decodeURI(uri.pathname),
+		SCRIPT_FILENAME: desc.fsPath.replace(/\\/g, '\/'),
+		SERVER_ROOT: root,
+		DOCUMENT_ROOT: root,
+		CONTEXT_DOCUMENT_ROOT: root,
 		SERVER_PORT: req.socket.localPort,
 		SERVER_ADDR: req.socket.localAddress,
 		REMOTE_ADDR: req.socket.remoteAddress,
-		REQUEST_METHOD: req.method,
-		SERVER_SIGNATURE: this.info,
-		SERVER_SOFTWARE: this.info,
-		//PATH_INFO, // shouldn't be set because it along with SCRIPT_NAME affects PHP_SELF
+		REMOTE_PORT: req.socket.remotePort,
+		SERVER_SIGNATURE: `<address>${SERVER_SOFTWARE}</address>`,
+		SERVER_SOFTWARE,
+		//PATH_INFO: '/',
 	}
+
+	// Passing headers from req.
 	var headers = req.headers
-	Object.keys(headers)
-		.filter(header => !header.startsWith(':')) // HTTP2 is to be solved sometime in the future. 
-		.forEach(header => env['HTTP_' + kebabToSnake(header)] = headers[header])
+	for (var name in req.headers) {
+		if (name.startsWith(':'))
+			continue // HTTP2 is to be solved sometime in the future. 
+		env['HTTP_' + name.toUpperCase().replace(/-/g, '_')] = headers[name]
+	}
+	if (req.headers.authorization)
+		env.AUTH_TYPE = req.headers.authorization.split(' ')[0]
+
+	// These are critical for POST requests with body.
+	// PHP Wouldn't know how long of a body to expect without content length.
+	if (req.headers['content-length'])
+		env.CONTENT_LENGTH = req.headers['content-length']
+	if (req.headers['content-type'])
+		env.CONTENT_TYPE = req.headers['content-type']
+	//if (req.headers['transfer-encoding'])
+	//	env.TRANSFER_ENCODING = req.headers['transfer-encoding']
+
+	// Adding custom user headers.
 	if (this.cgiEnv)
 		Object.assign(env, cgiEnv)
+
 	return env
-}
-
-
-function kebabToSnake(string) {
-	return string.toUpperCase().replace(/-/g, '_')
 }
