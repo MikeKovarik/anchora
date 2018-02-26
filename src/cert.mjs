27650d1d2f26bae478c01401412a04c21a29c7d2
Mike <kenr.mk@gmail.com>
2018-02-26 01:41:18 +0100

    cleanup in cert.mjs

--- src/cert.mjs
+++ src/cert.mjs
@@ -2,10 +2,9 @@ import selfsigned from 'selfsigned'
 import cp from 'child_process'
 import path from 'path'
 import util from 'util'
-import {fs, exec} from './util.mjs'
+import {fs, debug, exec} from './util.mjs'
 import {ensureDirectory} from './files.mjs'
-var {promisify} = util
-selfsigned.generate = promisify(selfsigned.generate)
+selfsigned.generate = util.promisify(selfsigned.generate)
 
 
 // NOTE: Node's HTTPS and HTTP2 classes accept object with {key, cert} properties
@@ -13,18 +12,18 @@ selfsigned.generate = promisify(selfsigned.generate)
 //       are used in the options object.
 
 export async function loadOrGenerateCertificate() {
-	this.debug && console.log('Loading or generating certificate for use in HTTPS or HTTP2')
+	debug('Loading or generating certificate for use in HTTPS or HTTP2')
 	try {
 		await this.loadCertificate()
-		this.debug && console.log('Certificate loaded')
+		debug('Certificate loaded')
 	} catch(err) {
 		await this.generateCertificate()
-		this.debug && console.log('Certificate geneated')
+		debug('Certificate geneated')
 		try {
 			await this.storeCertificate()
-			this.debug && console.log('Certificate stored')
+			debug('Certificate stored')
 			await this.installCertificate()
-			this.debug && console.log('Certificate installed')
+			debug('Certificate installed')
 		} catch(err) {
 			throw new Error(`certificate could not be loaded nor created, '${this.key}' '${this.cert}'`)
 		}
