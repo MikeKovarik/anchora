7098267498a47e1e1220a85363a575d23de79f5b
Mike <kenr.mk@gmail.com>
2018-09-07 00:30:32 +0200

    bust cache in folder browser

--- src/serve-folder.mjs
+++ src/serve-folder.mjs
@@ -13,12 +13,6 @@ var htmlDataGlobal
 export async function setupFolderBrowser() {
 	debug('setupFolderBrowser()')
 
-	var server = this
-/*
-	server.use((req, res) => {
-		debug('-', req.method, req.headers.host, req.url)
-	})
-*/
 	var promises = ['./folder-browser.html', './folder-browser.css', './folder-browser.js']
 		.map(name =>  path.join(__dirname, name))
 		.map(filePath => fs.readFile(filePath))
@@ -34,15 +28,39 @@ export async function setupFolderBrowser() {
 
 export async function serveFolder(req, res) {
 	debug('# serveFolder', req.desc.url)
-	var {fsPath, url} = req.desc
-	var names = await fs.readdir(fsPath)
-	var promises = names.map(name => this.openDescriptor(path.posix.join(url, name)))
+
+	if (!req.desc.folder) return
+	var url = req.safeUrl
+
+	// Try to look for index.html within the folder and redirect to it if it exists.
+	let indexUrl = path.join(url, this.indexFile)
+	let indexDesc = await this.openDescriptor(indexUrl)
+	if (indexDesc.exists) {
+		req.desc = indexDesc
+		return
+	}
+
+	// Render the folder
+	var names = await fs.readdir(req.desc.fsPath)
+	var promises = names
+		.map(name => path.posix.join(url, name))
+		.map(folderPath => this.openDescriptor(folderPath))
 	var descriptors = await Promise.all(promises)
 	var contentList = {url, descriptors}
+	// Prevent caching.
+	// WARNING: Necessary for json. Otherwise browser might use the json request and use that instead
+	//          of html render (neither older cached, nor new request) when pressing back button.
+	res.setHeader('cache-control', 'no-cache, no-store, must-revalidate')
+	res.setHeader('pragma', 'no-cache')
+	res.setHeader('expires', '0')
 	// Render the list as either JSON (if requested) or as HTML (by default), into the template.
 	if (req.headers.accept === 'application/json') {
 		res.json(contentList)
 	} else {
+		if (!htmlDataGlobal) {
+			// Fetch the folder browser html, css and js for the first time
+			await setupFolderBrowser()
+		}
 		var json = JSON.stringify(contentList)
 		var html = htmlDataGlobal.replace(replacePhrase, '= ' + json)
 		res.html(html)
